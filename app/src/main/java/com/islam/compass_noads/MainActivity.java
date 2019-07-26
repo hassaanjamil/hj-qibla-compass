510742346e842bf3d5c768c57c0c20611c13770e
hassanjamiltpl <hassan.jamil@tplmaps.com>
2019-07-26 17:55:58 +0500

    - Updated version 0.2.1 - Added latest APK, screenshot

--- app/src/main/java/com/islam/compass_noads/MainActivity.java
+++ app/src/main/java/com/islam/compass_noads/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 import com.hassanjamil.hqibla.CompassActivity;
-import com.hassanjamil.hqibla.Constants;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,14 +19,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CompassActivity.class);
-                intent.putExtra(Constants.COMPASS_BG_COLOR, "#FFFFFF");
+                /*intent.putExtra(Constants.COMPASS_BG_COLOR, "#FFFFFF");
                 intent.putExtra(Constants.TOOLBAR_BG_COLOR, "#FFFFFF");
                 intent.putExtra(Constants.TOOLBAR_TITLE_COLOR, "#000000");
                 intent.putExtra(Constants.ANGLE_TEXT_COLOR, "#000000");
-                //intent.putExtra(Constants.TOOLBAR_NAV_ICON, R.mipmap.ic_launcher);
                 intent.putExtra(Constants.DRAWABLE_DIAL, R.drawable.dial);
                 intent.putExtra(Constants.DRAWABLE_QIBLA, R.drawable.ic_arrow_back);
-                intent.putExtra(Constants.BOTTOM_IMAGE_VISIBLE, View.GONE);
+                intent.putExtra(Constants.BOTTOM_IMAGE_VISIBLE, View.GONE);*/
                 startActivity(intent);
             }
         });
